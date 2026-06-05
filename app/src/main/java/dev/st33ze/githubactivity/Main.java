af43ee12b4839e17e253526a75d66b9a81b91c03
st33ze <paweltrojnara@gmail.com>
2026-06-05 12:07:12 +0200

    feat: take username and print it out, or inform about usage if no username

--- app/src/main/java/dev/st33ze/githubactivity/Main.java
+++ app/src/main/java/dev/st33ze/githubactivity/Main.java
@@ -3,6 +3,13 @@ package dev.st33ze.githubactivity;
 public class Main {
   
   public static void main(String[] args) {
-    System.out.println("Initial app setup");
+    if (args.length == 0) {
+      System.out.println("Usage: github-activity <username>");
+      return;
+    }
+
+    String username = args[0];
+
+    System.out.println(username);
   }
 }
