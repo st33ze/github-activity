a391d4cfbcfcc0ddeed3d2dad1e4f8375076da1b
st33ze <paweltrojnara@gmail.com>
2026-06-16 12:08:52 +0200

    test(Output): add formatActivity tests for every tracked activity

---
+++ src/test/java/dev/st33ze/githubactivity/OutputTest.java
@@ -0,0 +1,94 @@
+package dev.st33ze.githubactivity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OutputTest {
+  
+  @Test
+  void formatPushEvent() {
+    Output output = new Output();
+    
+    UserActivity[] activities = {
+      new UserActivity(
+        "PushEvent",
+        new Repo("t3ster/newRepo"),
+        "date"
+      )
+    };
+
+    String result = output.format("t3ster", activities);
+
+    assertTrue(result.contains("Pushed commits to t3ster/newRepo"));
+  }
+
+  @Test
+  void formatCreateEvent() {
+    Output output = new Output();
+
+    UserActivity[] activities = {
+      new UserActivity(
+        "CreateEvent",
+        new Repo("t3ster/newRepo"),
+        "date"
+      )
+    };
+
+    String result = output.format("t3ster", activities);
+
+    assertTrue(result.contains("Created a new repository t3ster/newRepo"));
+  }
+
+  @Test
+  void formatForkEvent() {
+    Output output = new Output();
+
+    UserActivity[] activities = {
+      new UserActivity(
+        "ForkEvent",
+        new Repo("t3ster/ForkedRepo"),
+        "date"
+      )
+    };
+
+    String result = output.format("t3ster", activities);
+
+    assertTrue(result.contains("Forked repository t3ster/ForkedRepo"));
+  }
+
+  @Test
+  void formatWatchEvent() {
+    Output output = new Output();
+
+    UserActivity[] activities = {
+      new UserActivity(
+        "WatchEvent",
+        new Repo("t3ster/StarredRepo"),
+        "date"
+      )
+    };
+
+    String result = output.format("t3ster", activities);
+
+    assertTrue(result.contains("Starred t3ster/StarredRepo"));
+  }
+
+  @Test
+  void formatIssuesEvent() {
+    Output output = new Output();
+
+    UserActivity[] activities = {
+      new UserActivity(
+        "IssuesEvent",
+        new Repo("t3ster/IssuedRepo"),
+        "date"
+      )
+    };
+
+    String result = output.format("t3ster", activities);
+
+    assertTrue(result.contains("Opened a new issue in t3ster/IssueRepo"));
+  }
+
+}
