1180652f13906179c0765452d27c6a9e5c57f9d9
st33ze <paweltrojnara@gmail.com>
2026-06-20 13:18:51 +0200

    fix(UserActivity): createdAt field is null, because github api uses created_at as name

--- src/main/java/dev/st33ze/githubactivity/UserActivity.java
+++ src/main/java/dev/st33ze/githubactivity/UserActivity.java
@@ -1,10 +1,11 @@
 package dev.st33ze.githubactivity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record UserActivity(
   String type,
   Repo repo,
-  String createdAt
+  @JsonProperty("created_at") String createdAt
 ) {}
